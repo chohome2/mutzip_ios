9ec4416a6d48650776f6c3ce5df3f29933cbd9d4
taeho.cho <chohome2@gmail.com>
2014-05-13 16:57:48 +0900

    Initial Commit

---
+++ mutzip/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  mutzip
+//
+//  Created by taeho.cho on 14. 5. 13..
+//  Copyright (c) 2014년 taeho.cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ mutzip/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  mutzip
+//
+//  Created by taeho.cho on 14. 5. 13..
+//  Copyright (c) 2014년 taeho.cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
